7776d12df2656b0d1fa7832dd863613a77e8a089
Marcelo Silva <marcelos89@icloud.com>
2022-10-20 00:02:10 +0100

    upload pic players rankingMarket

--- Marcelo.WorldCupQatar22/Features/Ranking/Model/Players/Player.swift
+++ Marcelo.WorldCupQatar22/Features/Ranking/Model/Players/Player.swift
@@ -28,35 +28,69 @@ let player: [Player] = [
     Player(image: #imageLiteral(resourceName: "Screen Shot 2022-10-19 at 01.30.08"), name: "Dusan Vlahovic", age: "22", nation: "🇷🇸", valueMarket: "€85.00m"),
     Player(image: #imageLiteral(resourceName: "Screen Shot 2022-10-19 at 01.30.23"), name: "Bruno Fernandes", age: "28", nation: "🇵🇹", valueMarket: "€85.00m"),
     Player(image: #imageLiteral(resourceName: "Screen Shot 2022-10-19 at 01.30.29"), name: "Kevin De Bruyne", age: "31", nation: "🇧🇪", valueMarket: "€85.00m"),
-    Player(image: #imageLiteral(resourceName: "Screen Shot 2022-10-19 at 01.30.29"), name: "Jamal Musiala", age: "19", nation: "🇩🇪", valueMarket: "€85.00m"),
+    Player(image: #imageLiteral(resourceName: "Screen Shot 2022-10-19 at 23.10.21"), name: "Jamal Musiala", age: "19", nation: "🇩🇪", valueMarket: "€80.00m"),
+    Player(image: #imageLiteral(resourceName: "Screen Shot 2022-10-19 at 23.11.58"),name: "Declan Rice", age: "23", nation: "🏴󠁧󠁢󠁥󠁮󠁧󠁿", valueMarket: "€80.00m"),
+    Player(image: #imageLiteral(resourceName: "Screen Shot 2022-10-19 at 23.14.07"),name: "Trent Arnold", age: "24", nation: "🏴󠁧󠁢󠁥󠁮󠁧󠁿", valueMarket: "€80.00m"),
+    Player(image: #imageLiteral(resourceName: "Screen Shot 2022-10-19 at 23.14.58"),name: "Bernardo Silva", age: "28", nation: "🇵🇹", valueMarket: "€80.00m"),
+    Player(image: #imageLiteral(resourceName: "Screen Shot 2022-10-19 at 23.16.10"),name: "Joshua Kimmich", age: "27", nation: "🇩🇪", valueMarket: "€80.00m"),
+    Player(image: #imageLiteral(resourceName: "Screen Shot 2022-10-19 at 23.16.17"),name: "Antony", age: "22", nation: "🇧🇷", valueMarket: "€75.00m"),
+    Player(image: #imageLiteral(resourceName: "Screen Shot 2022-10-19 at 23.18.08"),name: "Lautaro Martinez", age: "25", nation: "🇦🇷", valueMarket: "€75.00m"),
+    Player(image: #imageLiteral(resourceName: "Screen Shot 2022-10-19 at 23.20.45"),name: "Rúben Dias", age: "25", nation: "🇵🇹", valueMarket: "€75.00m"),
+    Player(image: #imageLiteral(resourceName: "Screen Shot 2022-10-19 at 23.21.50"),name: "Heung - min Son", age: "30", nation: "🇰🇷", valueMarket: "€75.00m"),
+    Player(image: #imageLiteral(resourceName: "Screen Shot 2022-10-19 at 23.21.57"),name: "Neymar", age: "30", nation: "🇧🇷", valueMarket: "€75.00m"),
+    Player(image: #imageLiteral(resourceName: "Screen Shot 2022-10-19 at 23.29.27"),name: "Mason Mount", age: "23", nation: "🏴󠁧󠁢󠁥󠁮󠁧󠁿", valueMarket: "€75.00m"),
+    Player(image: #imageLiteral(resourceName: "Screen Shot 2022-10-19 at 23.22.08"), name: "Gavi", age: "18", nation: "🇪🇸", valueMarket: "€70.00m"),
+    Player(image: #imageLiteral(resourceName: "Screen Shot 2022-10-19 at 23.22.28"),name: "João Félix", age: "22", nation: "🇵🇹", valueMarket: "€70.00m"),
+    Player(image: #imageLiteral(resourceName: "Screen Shot 2022-10-19 at 23.27.58"), name: "Rodrygo", age: "21", nation: "🇧🇷", valueMarket: "€70.00m"),
+    Player(image: #imageLiteral(resourceName: "Screen Shot 2022-10-19 at 23.32.13"),name: "Raheem Sterling", age: "27", nation: "🏴󠁧󠁢󠁥󠁮󠁧󠁿", valueMarket: "€70.00m"),
+    Player(image: #imageLiteral(resourceName: "Screen Shot 2022-10-19 at 23.28.05"),name: "Marquinhos", age: "28", nation: "🇧🇷", valueMarket: "€70.00m"),
+    Player(image: #imageLiteral(resourceName: "Screen Shot 2022-10-19 at 23.28.19"),name: "Romelu Lukaku", age: "29", nation: "🇧🇪", valueMarket: "€70.00m"),
+    Player(image: #imageLiteral(resourceName: "Screen Shot 2022-10-19 at 23.36.08.png"),name: "Thibaut Courtois", age: "30", nation: "🇧🇪", valueMarket: "€60.00m"),
+    Player(image: #imageLiteral(resourceName: "Screen Shot 2022-10-19 at 23.28.31"),name: "G. Jesus", age: "25", nation: "🇧🇷", valueMarket: "€65.00m"),
+    Player(image: #imageLiteral(resourceName: "Screen Shot 2022-10-19 at 23.35.20"), name: "Éder Militão", age: "24", nation: "🇧🇷", valueMarket: "€60.00m"),
+    Player(image: #imageLiteral(resourceName: "Screen Shot 2022-10-19 at 23.35.49"), name: "Frenkie de Jong", age: "25", nation: "🇳🇱", valueMarket: "€60.00m"),
+    Player(image: #imageLiteral(resourceName: "Screen Shot 2022-10-19 at 23.36.01"), name: "Sadio Mané", age: "30", nation: "🇸🇳", valueMarket: "€60.00m"),
+    Player(image: #imageLiteral(resourceName: "Screen Shot 2022-10-19 at 23.46.53"), name: "Alisson", age: "29", nation: "🇧🇷", valueMarket: "€60.00m"),
+    Player(image: #imageLiteral(resourceName: "Screen Shot 2022-10-19 at 23.36.19"), name: "Eduardo Camavinga", age: "19", nation: "🇫🇷", valueMarket: "€55.00m"),
+    Player(image: #imageLiteral(resourceName: "Screen Shot 2022-10-19 at 23.48.04"), name: "Casemiro", age: "30", nation: "🇧🇷", valueMarket: "€50.00m"),
+    Player(image: #imageLiteral(resourceName: "Screen Shot 2022-10-19 at 23.47.58"), name: "Lionel Messi", age: "35", nation: "🇦🇷", valueMarket: "€50.00m"),
+    Player(image: #imageLiteral(resourceName: "Screen Shot 2022-10-19 at 23.48.27"), name: "Lewandowski", age: "33", nation: "🇵🇱", valueMarket: "€50.00m"),
+    Player(image: #imageLiteral(resourceName: "Screen Shot 2022-10-19 at 23.56.07"), name: "Karim Benzema", age: "34", nation: "🇫🇷", valueMarket: "€30.00m"),
+    Player(image: #imageLiteral(resourceName: "Screen Shot 2022-10-19 at 23.57.06"), name: "Cristiano Ronaldo", age: "37", nation: "🇵🇹", valueMarket: "€20.00m"),
+    
+    
+    
+
+    
+    
     
     
 
 
 
-//    Player(image:#imageLiteral(),name: "Joshua Kimmich", age: "27", nation: "🇩🇪", valueMarket: "€80.00m"),
-//    Player(image:#imageLiteral(),name: "Bernardo Silva", age: "27", nation: "🇵🇹", valueMarket: "€80.00m"),
-//    Player(image:#imageLiteral(),name: "Trent Arnold", age: "24", nation: "🏴󠁧󠁢󠁥󠁮󠁧󠁿", valueMarket: "€80.00m"),
-//    Player(image:#imageLiteral(),name: "Declan Rice", age: "23", nation: "🏴󠁧󠁢󠁥󠁮󠁧󠁿", valueMarket: "€75.00m"),
-//    Player(image:#imageLiteral(),name: "Antony", age: "22", nation: "🇧🇷", valueMarket: "€75.00m"),
-//    Player(image:#imageLiteral(),name: "Lautaro Martinez", age: "24", nation: "🇦🇷", valueMarket: "€75.00m"),
-//    Player(image:#imageLiteral(),name: "Rúben Dias", age: "25", nation: "🇵🇹", valueMarket: "€75.00m"),
-//    Player(image:#imageLiteral(),name: "Mason Mount", age: "23", nation: "🏴󠁧󠁢󠁥󠁮󠁧󠁿", valueMarket: "€75.00m"),
-//    Player(image:#imageLiteral(),name: "Heung - min Son", age: "30", nation: "🇰🇷", valueMarket: "€75.00m"),
-//    Player(image:#imageLiteral(),name: "Neymar", age: "30", nation: "🇧🇷", valueMarket: "€75.00m"),
-//    Player(image:#imageLiteral(), name: "Rodrygo", age: "21", nation: "🇧🇷", valueMarket: "€70.00m"),
-//    Player(image:#imageLiteral(),name: "João Félix", age: "22", nation: "🇵🇹", valueMarket: "€70.00m"),
-//    Player(image:#imageLiteral(),name: "Raheem Sterling", age: "27", nation: "🏴󠁧󠁢󠁥󠁮󠁧󠁿", valueMarket: "€70.00m"),
-//    Player(image:#imageLiteral(),name: "Romelu Lukaku", age: "29", nation: "🇧🇪", valueMarket: "€70.00m"),
-//    Player(image:#imageLiteral(),name: "Marquinhos", age: "27", nation: "🇧🇷", valueMarket: "€70.00m"),
-//    Player(image:#imageLiteral(), name: "Gavi", age: "18", nation: "🇪🇸", valueMarket: "€70.00m"),
-//    Player(image:#imageLiteral(),name: "G. Jesus", age: "25", nation: "🇧🇷", valueMarket: "€65.00m"),
-//    Player(image:#imageLiteral(), name: "Éder Militão", age: "24", nation: "🇧🇷", valueMarket: "€60.00m"),
-//    Player(image:#imageLiteral(), name: "Sadio Mané", age: "30", nation: "🇸🇳", valueMarket: "€60.00m"),
-//    Player(image:#imageLiteral(), name: "Casemiro", age: "30", nation: "🇧🇷", valueMarket: "€50.00m"),
-//    Player(image:#imageLiteral(), name: "Lionel Messi", age: "35", nation: "🇦🇷", valueMarket: "€50.00m"),
-//    Player(image:#imageLiteral(), name: "Karim Benzema", age: "34", nation: "🇫🇷", valueMarket: "€30.00m")
-//    Player(image:#imageLiteral(), name: "Cristiano Ronaldo", age: "37", nation: "🇵🇹", valueMarket: "€20.00m"),
+
+
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //
 //
 
@@ -64,22 +98,10 @@ let player: [Player] = [
 
 
 
-//    Player(image:#imageLiteral(), name: "Federico Chiesa", age: "24", nation: "🇮🇹", valueMarket: "€70.00m"),
-//    Player(image:#imageLiteral(), name: "Frenkie de Jong", age: "24", nation: "🇳🇱", valueMarket: "€70.00m"),
-//    Player(image:#imageLiteral(), name: "Matthijs de Ligt", age: "22", nation: "🇳🇱", valueMarket: "€70.00m"),
-//    Player(image: #imageLiteral(),name: "Kai Havertz", age: "22", nation: "🇩🇪", valueMarket: "€75.00m"),
-//    Player(image:#imageLiteral(), name: "Alisson", age: "29", nation: "🇧🇷", valueMarket: "€60.00m"),
-//    Player(image:#imageLiteral(), name: "Casemiro", age: "30", nation: "🇧🇷", valueMarket: "€50.00m"),
-//
-//
-//    Player(image:#imageLiteral(), name: "Eduardo Camavinga", age: "19", nation: "🇫🇷", valueMarket: "€55.00m"),
-//    Player(image:#imageLiteral(), name: "Piotr Zielinski", age: "27", nation: "🇵🇱", valueMarket: "€50.00m"),
-//
-//    Player(image:#imageLiteral(), name: "Robert Lewandowski", age: "33", nation: "🇵🇱", valueMarket: "€50.00m"),
-//    Player(image:#imageLiteral(), name: "Kanté", age: "31", nation: "🇫🇷", valueMarket: "€55.00m"),
-//    Player(image:#imageLiteral(), name: "Virgil van Dijk", age: "30", nation: "🇳🇱", valueMarket: "€55.00m"),
-//
-//
-//    Player(image:#imageLiteral(), name: "Edouard Mendy", age: "30", nation: "🇸🇳", valueMarket: "€32.00m"),
+
+
+
+
+
 
 ]
